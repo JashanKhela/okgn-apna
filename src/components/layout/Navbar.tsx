e1bb71642fb359f312771910a88c12e496ae5f32
Jashan Khela <jashan.khela@hotmail.com>
2025-04-01 15:13:14 -0700

    Update Hamburger Menu

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -33,7 +33,6 @@ export function Navbar() {
     }
     fetchData();
   }, []);
- console.log(data)
   return (
     <header className="sticky top-0 z-50 bg-white border-b border-gray-200">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-6 flex justify-between items-center h-32">
@@ -60,7 +59,7 @@ export function Navbar() {
         <div className="md:hidden">
           <Sheet open={open} onOpenChange={setOpen}>
             <SheetTrigger className="text-gray-700 p-2 rounded-md hover:bg-gray-100">
-              <Menu size={22} />
+              <Menu size={56} />
             </SheetTrigger>
             <SheetContent side="right" className="bg-white px-6 py-6 w-[260px]">
               <div className="flex items-center justify-between mb-6">
